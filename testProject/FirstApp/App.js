f39070ffbd856924b85d4408475bc81effb6b61d
chakane0 <chakanezshegog@gmail.com>
2024-05-31 17:14:48 -0400

    first commit / check devices

--- testProject/FirstApp/App.js
+++ testProject/FirstApp/App.js
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, TextInput, View } from 'react-native';
+ 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View>
+        <TextInput placeholder='your course goal'/>
+        <Button title='Add Goal'/>
+      </View>
+      <view>
+        <Text>List of goals...</Text>
+      </view>
     </View>
   );
 }
-
+ 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  appContainer: {
+    padding: 50
+  }
+})
